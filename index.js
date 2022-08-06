942048e8ac7bd838b957db2dd9e8519198088bf5
piyush81022 <109734844+piyush81022@users.noreply.github.com>
2022-08-06 09:15:06 -0700

    updateindex

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("this is coder piyush ranjan");
+
+a=5;
+b=4;
+console.log(c);
+
+
+
+
+
+
+console.log("this is coder piyush ranjan")
+git hub desktop is only run when a internate connection is establish
